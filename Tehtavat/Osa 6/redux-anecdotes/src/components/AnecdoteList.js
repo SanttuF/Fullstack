1c63554320e11d83dcf0b24519afca4f6a0ac6ab
SanttuF <santtu.f@gmail.com>
2023-05-07 01:23:44 +0300

    filter

--- Tehtavat/Osa 6/redux-anecdotes/src/components/AnecdoteList.js
+++ Tehtavat/Osa 6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes.filter(a => a.content.toLowerCase().includes(state.filter)))
   const dispatch = useDispatch()
 
   const vote = id => {
--- Tehtavat/Osa 6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Tehtavat/Osa 6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,10 +19,7 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
+const anecdoteReducer = (state = initialState, action) => {
   switch(action.type) {
     case 'ADD_ANECDOTE':
       return [...state, action.payload]
@@ -55,4 +52,4 @@ export const createAnecdote = content => ({
   }
 })
 
-export default reducer
+export default anecdoteReducer
