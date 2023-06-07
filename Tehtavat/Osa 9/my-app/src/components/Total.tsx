ecbfea18ff30590d06b7753a05b5f70f2312dc5b
SanttuF <santtu.f@gmail.com>
2023-06-07 19:22:45 +0300

    8d

---
+++ Tehtavat/Osa 9/my-app/src/components/Total.tsx
@@ -0,0 +1,12 @@
+const Total = ({
+  courseParts,
+}: {
+  courseParts: { name: string; exerciseCount: number }[];
+}) => (
+  <p>
+    Number of exercises{" "}
+    {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+  </p>
+);
+
+export default Total;
